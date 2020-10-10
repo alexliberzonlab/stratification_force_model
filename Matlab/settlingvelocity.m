43d9cde708181401b806783780f88c7af6efdac4
lillyverso <lillyverso@gmail.com>
2020-10-10 09:49:28 +0200

    Update settlingvelocity.m
    The White's drag formula was updated

--- Matlab/settlingvelocity.m
+++ Matlab/settlingvelocity.m
@@ -1,6 +1,6 @@
 function ws = settlingvelocity(rhop,rhof,g,d,nu)
 
-Cd = @(Re)  0.25 + (24./Re) + (6./(1+Re.^(0.5)));  % Drag law according to White 1974 
+Cd = @(Re)  0.4 + (24./Re) + (6./(1+Re.^(0.5)));  % Drag law according to White 1974 
 
 Ap=(pi*d^2)/4;              % surface of the sphere - m2
 Vp=(pi*d^3)/6;              % volume  of the sphere - m3
